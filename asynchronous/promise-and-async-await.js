ddbe11ff0247936bbe58ae4f62ffe5b947e19d81
Fitri Annisa Ahlul Jannah <56632892+ftrannisa@users.noreply.github.com>
2020-09-14 15:05:50 +0700

    Update promise-and-async-await.js

--- asynchronous/promise-and-async-await.js
+++ asynchronous/promise-and-async-await.js
@@ -1,41 +1,19 @@
-const request = require("request");
-const options = {
-  url: "https://api.github.com/repos/request/request",
-  headers: {
-    "User-Agent": "request",
-  },
-};
-
-// CALLBACK
-// function callback(error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//     const info = JSON.parse(body);
-//     console.log(info.name);
-//     console.log(info.stargazers_count + " Stars");
-//     console.log(info.forks_count + " Forks");
-//   }
-// }
-// request(options, callback);
-
-// PROMISE
-const reqGithub = () => {
-  return new Promise((resolve, reject) => {
-    request(options, (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        const repo = JSON.parse(body);
-        resolve(repo);
-      } else {
-        reject("Error");
-      }
-    });
-  });
-};
-console.log(reqGithub());
-
-// ASYNC AWAIT
-
 const fetch = require("node-fetch");
-const reqAsync = async () => {
+
+const requestRepo = () => {
+  return fetch("https://api.github.com/repos/request/request")
+    .then((response) => response.json())
+    .then((json) => {
+      return json.stargazers_count;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+requestRepo().then((e) => console.log(e));
+
+//-----
+const reqRepo = async () => {
   try {
     let response = await fetch("https://api.github.com/repos/request/request");
     let json = await response.json();
@@ -44,4 +22,4 @@ const reqAsync = async () => {
     console.error(error);
   }
 };
-console.log(reqAsync());
+console.log(reqRepo());
